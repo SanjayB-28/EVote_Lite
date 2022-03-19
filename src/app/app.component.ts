dd8788a0200fd13bb20d19a5cd8d38282cf6b129
Sanjay <sanjaysmack@gmail.com>
2022-03-19 23:03:14 +0530

    Poll Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   showForm = false;
+
+  polls = [
+    {
+      question: 'Do you like Dogs or Cats?',
+      image: 'https://marvel-b1-cdn.bc0a.com/f00000000052994/www.hillspet.com/content/dam/cp-sites/hills/hills-pet/en_us/exported/pet-care/Skyword/images/basset-hound-sleeps-on-sleeping-tabby-cat_308513_ref.png',
+      votes: [0,5,7],
+      voted: true,
+    },
+    {
+      question: 'Which is the Best Season?',
+      image: 'https://c.tadst.com/gfx/900x506/four-seasons.jpg?1',
+      votes: [1,6,4],
+      voted: false,
+    },
+  ];
 }
